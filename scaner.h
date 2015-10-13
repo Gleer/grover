87a11d4f4c831c32e7178cbf3e814c776cd78059
Gleer <gleer.hunt@gmail.com>
2015-10-13 13:11:28 +0200

    OK

--- scaner.h
+++ scaner.h
@@ -55,7 +55,7 @@
 #define retezec       44  // string - retezec znaku ""
 #define id            45  // identifikator 
 
-// chyba 46
+// chyba 
 
 #define Typ_CHYBA     46  // posilame v pripade chyby (pri spatne alokaci)
 
